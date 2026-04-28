f71a9fe163a44c9adfbba778ba659cdd719a8158
apexutravel <apexutravel@gmail.com>
2026-04-28 12:50:21 -0700

    fix nuevos permisos de instagram

--- src/facebook/facebook.service.ts
+++ src/facebook/facebook.service.ts
@@ -126,9 +126,8 @@ export class FacebookService {
       'pages_show_list',
       'pages_manage_metadata',
       'pages_read_engagement',
-      'instagram_basic',
-      'instagram_manage_messages',
-      'instagram_manage_comments',
+      'instagram_content_publish',
+      'instagram_manage_insights',
     ].join(',');
 
     return `https://www.facebook.com/v21.0/dialog/oauth?client_id=${config.appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}&response_type=code&state=${tenantId}`;
